660e64ccbe32b30910cf17e3c6406513ad660bfd
Alisa Kuzmina <30773217+arisuaiko@users.noreply.github.com>
2018-09-10 17:10:58 +0300

    Лекция 4.5. Формирование коллекции

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -2,12 +2,16 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GroupHelper extends HelperBase {
 
-    public GroupHelper(WebDriver wd) {
+  public GroupHelper(WebDriver wd) {
         super(wd);
     }
 
@@ -60,4 +64,15 @@ public class GroupHelper extends HelperBase {
     public int getGroupCount() {
         return wd.findElements(By.name("selected[]")).size();
     }
+
+  public List<GroupData> getGroupList() {
+    List<GroupData> groups = new ArrayList<GroupData>();
+    List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+    for (WebElement element : elements) {
+      String name = element.getText();
+      GroupData group = new GroupData(name, null, null);
+      groups.add(group);
+    }
+    return groups;
+  }
 }
